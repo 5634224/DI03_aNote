bf6ff924f212791751c1745f898f3360c6bd2713
santiago <5634224@alu.murciaeduca.es>
2023-12-29 13:33:58 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.santiago.di03_anote {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.santiago.di03_anote to javafx.fxml;
+    exports com.santiago.di03_anote;
+}
